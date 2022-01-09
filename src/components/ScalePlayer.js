0ebdf1f4adaf6eadd5783229ca26c1771dea389d
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-09 11:44:53 -0800

    render static UI

--- src/components/ScalePlayer.js
+++ src/components/ScalePlayer.js
@@ -1,7 +1,19 @@
 import React, { useState } from "react";
 
 const ScalePlayer = () => {
-  return <h2>scale player</h2>;
+  return (
+    <div className="container">
+      <h2>C Major Pentatonic</h2>
+      <p>C, D, F, G, Bb</p>
+      <select>
+        <option>C</option>
+      </select>
+      <select>
+        <option>C Major Pentatonic</option>
+      </select>
+      <button>Start</button>
+    </div>
+  );
 };
 
 export default ScalePlayer;
